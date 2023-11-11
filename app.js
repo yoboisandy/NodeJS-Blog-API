2ec331efcf6540d7e3ad319db6906e5d6b0752b2
Sandeep Sharma <1949sandeepsharma52@gmail.com>
2023-11-11 16:45:11 +0545

    database setup and blog table

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+require("./model/index");
+
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
